ed27da714bd420e33efaee8a25b6daf323680d6d
Jeremiah Aaron <jeremiah.aaron117@gmail.com>
2022-06-23 12:58:17 +0700

    Now can play a whole playlist, added clear queue command, added queue pagination

--- jumbot.py
+++ jumbot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import youtube_dl
 import os
 from dotenv import load_dotenv
+from math import ceil
 
 # Initialize bot with prefix '=' and queue dictionary
 bot = commands.Bot(command_prefix='=')
@@ -42,7 +43,27 @@ async def check_queue(ctx, server_id):
         await ctx.message.channel.send(embed=embed_msg)
 
 
-# Command to play song with 'p'
+# Function to queue every song in a playlist
+async def queue_playlist(ctx, info):
+    server_id = ctx.message.guild.id
+    requester = ctx.message.author.name
+
+    # Loops through every song in the playlist and add them to the queue
+    for i in range(0, len(info['entries'])):
+        video_link = info['entries'][i]['formats'][0]['url']
+        song_id = info['entries'][i].get('id')
+        uploader = info['entries'][i].get('uploader')
+        song_title = info['entries'][i].get('title')
+        duration = info['entries'][i].get('duration')
+        if server_id in queue_dict:
+            queue_dict[server_id].append({'id': song_id, 'uploader': uploader, 'title': song_title,
+                                          'duration': duration, 'url': video_link, 'requester': requester})
+        else:
+            queue_dict[server_id] = [{'id': song_id, 'uploader': uploader, 'title': song_title,
+                                      'duration': duration, 'url': video_link, 'requester': requester}]
+
+
+# Command to play song with 'play'
 @bot.command(name="play", help="Plays or queues a song")
 async def play(ctx, *, url: str):
     voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
@@ -60,41 +81,38 @@ async def play(ctx, *, url: str):
 
     ydl_opts = {
         'format': 'bestaudio/best',
-        'noplaylist': 'True',
         'default_search': 'auto'
     }
+    is_playlist = False
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        # Check whether user searched by keywords or played using URL
-        if 'entries' in info:
-            video_link = info['entries'][0]['formats'][0]['url']
-            song_id = info['entries'][0].get('id')
-            uploader = info['entries'][0].get('uploader')
-            song_title = info['entries'][0].get('title')
-            duration = info['entries'][0].get('duration')
-        elif 'formats' in info:
-            video_link = info['formats'][0]['url']
-            song_id = info.get('id')
-            uploader = info.get('uploader')
-            song_title = info.get('title')
-            duration = info.get('duration')
+        async with ctx.typing():
+            info = ydl.extract_info(url, download=False)
+
+            # Check whether user searched by keywords or played using URL
+            if 'entries' in info:
+                if len(info['entries']) > 1:
+                    is_playlist = True
+                    playlist_title = info['entries'][0]['playlist_title']
+                video_link = info['entries'][0]['formats'][0]['url']
+                song_id = info['entries'][0].get('id')
+                uploader = info['entries'][0].get('uploader')
+                song_title = info['entries'][0].get('title')
+                duration = info['entries'][0].get('duration')
+            elif 'formats' in info:
+                video_link = info['formats'][0]['url']
+                song_id = info.get('id')
+                uploader = info.get('uploader')
+                song_title = info.get('title')
+                duration = info.get('duration')
     requester = ctx.message.author.name
 
-    # Add song to queue if bot is already playing a song, else play the song
-    if voice_client.is_playing():
+    ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                      'options': '-vn'}
+
+    # Nested function to start playing a song
+    async def play_song():
         async with ctx.typing():
-            if server_id in queue_dict:
-                queue_dict[server_id].append({'id': song_id, 'uploader': uploader, 'title': song_title,
-                                              'duration': duration, 'url': video_link, 'requester': requester})
-            else:
-                queue_dict[server_id] = [{'id': song_id, 'uploader': uploader, 'title': song_title,
-                                          'duration': duration, 'url': video_link, 'requester': requester}]
-        await ctx.send('Added `{}` to the queue at number `{}`'.format(song_title, len(queue_dict[server_id])))
-    else:
-        async with ctx.typing():
-            ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-                              'options': '-vn'}
             voice_client.play(
                 await discord.FFmpegOpusAudio.from_probe(video_link, **ffmpeg_options),
                 after=lambda e:
@@ -106,6 +124,32 @@ async def play(ctx, *, url: str):
             embed_msg.add_field(name="Duration", value='{:02d}:{:02d}'.format(duration // 60, duration % 60), inline=True)
             embed_msg.add_field(name="Requested by", value=requester, inline=True)
         await ctx.message.channel.send(embed=embed_msg)
+
+    # If the URL is not a playlist and the bot is playing a song
+    if not is_playlist and voice_client.is_playing():
+        async with ctx.typing():
+            if server_id in queue_dict:
+                queue_dict[server_id].append({'id': song_id, 'uploader': uploader, 'title': song_title,
+                                              'duration': duration, 'url': video_link, 'requester': requester})
+            else:
+                queue_dict[server_id] = [{'id': song_id, 'uploader': uploader, 'title': song_title,
+                                          'duration': duration, 'url': video_link, 'requester': requester}]
+        await ctx.send('Added `{}` to the queue at number `{}`'.format(song_title, len(queue_dict[server_id])))
+    # If the URL is a playlist and the bot is playing a song
+    elif is_playlist and voice_client.is_playing():
+        async with ctx.typing():
+            await queue_playlist(ctx, info)
+        await ctx.send('Added `{}` playlist to the queue'.format(playlist_title))
+    # If the URL is a playlist and the bot is not playing a song
+    elif is_playlist and not voice_client.is_playing():
+        async with ctx.typing():
+            await queue_playlist(ctx, info)
+        await ctx.send('Added `{}` playlist to the queue'.format(playlist_title))
+        queue_dict[server_id].pop(0)
+        await play_song()
+    # If the URL is not a playlist and the bot is not playing a song
+    else:
+        await play_song()
 
 
 # Command to pause the current song
@@ -179,20 +223,49 @@ async def remove(ctx, index: int):
 
 # Command to display the queue
 @bot.command(name="queue", help="Displays the current queue of songs")
-async def queue(ctx):
+async def queue(ctx, page: int = None):
+    songs_per_page = 10  # Number of songs to be displayed in a queue page
     server_id = ctx.message.guild.id
-    if len(queue_dict) == 0 or len(queue_dict[server_id]) == 0:
+
+    # Check if there are songs in the queue
+    if len(queue_dict) == 0 or server_id not in queue_dict or len(queue_dict[server_id]) == 0:
         await ctx.send('```There are no songs in the queue currently```')
+        return
     else:
-        # Display queue using index and song title
-        display_str = "```Upcoming queue of songs:\n"
-        index = 1
-        for info in queue_dict[server_id]:
-            song_title = info.get('title')
-            display_str += '{}: {}\n'.format(index, song_title)
-            index += 1
-        display_str += "```"
-        await ctx.send(display_str)
+        total_page = ceil(len(queue_dict[server_id]) / songs_per_page)  # Calculate total page(s)
+
+    # Check every possible parameter input
+    if page is None:
+        page = total_page  # set displayed page to the last one if no parameter exists
+    else:
+        if page < 1:
+            page = 1
+        elif page > total_page:
+            page = total_page
+
+    # Print songs in the selected page
+    display_str = "```Queue page: {}/{}\n\n".format(page, total_page)
+    starting_index = songs_per_page * (page-1)  # starting index of songs in one page
+    for index in range(starting_index, starting_index + songs_per_page):
+        if index >= len(queue_dict[server_id]):
+            break
+        song_title = queue_dict[server_id][index].get('title')
+        display_str += '{}: {}\n'.format(index+1, song_title)
+        index += 1
+    display_str += "```"
+    await ctx.send(display_str)
+
+
+# Command to clear the queue
+@bot.command(name="clear", help="Clears the current song queue")
+async def clear(ctx):
+    async with ctx.typing():
+        server_id = ctx.message.guild.id
+        if len(queue_dict) == 0 or server_id not in queue_dict or len(queue_dict[server_id]) == 0:
+            await ctx.send('The queue is already empty')
+        else:
+            del queue_dict[server_id]
+    await ctx.send('Cleared the whole queue')
 
 
 # Print a message to the console when bot is ready
